6b306f33016c1cfc8f3080b30e45ee87520c69a2
dasbidyendu <143386290+dasbidyendu@users.noreply.github.com>
2025-04-10 08:26:01 +0530

    Update navbar and add Star background in homepage

---
+++ app/components/Starfield.tsx
@@ -0,0 +1,41 @@
+const StarField = () => {
+  const numberOfStars = 150;
+  const stars = [];
+
+  for (let i = 0; i < numberOfStars; i++) {
+    const top = `${Math.random() * 100}vh`;
+    const left = `${Math.random() * 100}vw`;
+    const size = `${Math.random() * 2 + 1}px`;
+    const opacity = Math.random() * 0.5 + 0.3;
+    const delay = `${Math.random() * 3}s`;
+    const twinkleDuration = `${2 + Math.random() * 3}s`; // 2–5s
+    const driftDuration = `${5 + Math.random() * 4}s`; // 8–12s
+
+    stars.push(
+      <div
+        key={i}
+        className="star"
+        style={{
+          top,
+          left,
+          width: size,
+          height: size,
+          opacity,
+          animationName: "twinkle, drift-horizontal",
+          animationDuration: `${twinkleDuration}, ${driftDuration}`,
+          animationDelay: `${delay}, 0s`,
+          animationDirection: "alternate",
+          animationTimingFunction: "ease-in-out",
+        }}
+      />
+    );
+  }
+
+  return (
+    <div className="star-field fixed w-screen h-screen top-0 left-0">
+      {stars}
+    </div>
+  );
+};
+
+export default StarField;
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
-import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import StarField from "./components/Starfield";
 
 export default function Home() {
   return (
-   <div className="w-screen h-screen   pt-10 bg-[#0e0217]">
-    <Navbar/>
-    <HeroSection/>
-
-
-   </div>
+    <div className="w-screen h-screen   pt-10 bg-[#0e0217]">
+      <Navbar />
+      <HeroSection />
+      <StarField />
+    </div>
   );
 }
